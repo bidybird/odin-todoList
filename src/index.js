6013280e24a3001dbceafe361c404e3283e681ff
Jonathan Shovak <shovakitup@gmail.com>
2022-11-03 14:43:47 -0500

    project factory function now displays content entered into a function made with  it, with function projectInfo

--- src/index.js
+++ src/index.js
@@ -4,4 +4,34 @@ const Project = (title, description, dueDate, priority, notes) => {
   this.dueDate = dueDate;
   this.priority = priority;
   this.notes = notes;
+
+  const sumOfProject = sumProject(title, description, dueDate, priority, notes);
+
+  const displayProject = displayContent(sumOfProject);
+
+  return {
+    projectInfo: displayProject,
+  };
 };
+
+//--------------------------------------------------
+
+const projectOne = Project(
+  "Buy Food",
+  "Go to HEB and buy chips and beer.",
+  "11/3/2022",
+  "Immediately",
+  "Also say thank you to the person working the cash register."
+);
+
+projectOne.projectInfo;
+
+//--------------------------------------------------
+
+function sumProject(title, description, dueDate, priority, notes) {
+  return `${title} ${description} ${dueDate} ${priority} ${notes}`;
+}
+
+function displayContent(content) {
+  return console.log(content);
+}
