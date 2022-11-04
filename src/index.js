237af30fa392828d3bde7cea37a3c298e5e41d9b
Jonathan Shovak <shovakitup@gmail.com>
2022-11-04 03:46:58 -0500

    adjusted project constructor to return all filled in portions as well as a sum function that may be removed

--- src/index.js
+++ src/index.js
@@ -1,37 +1,88 @@
-const Todo = (title, description, dueDate, priority, notes) => {
-  this.title = title;
+const projectArray = [];
+const todoTotalArray = [];
+//-------------------------------------
+//making a todo
+function Todo(task, description, dueDate, priority, notes) {
+  this.task = task;
   this.description = description;
   this.dueDate = dueDate;
   this.priority = priority;
   this.notes = notes;
 
-  const sumOfTodo = sumTodo(title, description, dueDate, priority, notes);
-
-  const displayTodo = displayContent(sumOfTodo);
+  const sumOfTodo = sumTodo(task, description, dueDate, priority, notes);
 
   return {
-    todoInfo: displayTodo,
+    todoInfo: sumOfTodo,
   };
-};
-
-//--------------------------------------------------
-
-const todoOne = Todo(
-  "Buy Food",
-  "Go to HEB and buy chips and beer.",
-  "11/3/2022",
-  "Immediately",
-  "Also say thank you to the person working the cash register."
-);
-
-todoOne.todoInfo;
-
-//--------------------------------------------------
-
-function sumTodo(title, description, dueDate, priority, notes) {
-  return `${title} ${description} ${dueDate} ${priority} ${notes}`;
 }
 
-function displayContent(content) {
-  return console.log(content);
+//--------------------------------------------------
+//functions that will be used many times, in objects made often
+//in Todo
+function sumTodo(task, description, dueDate, priority, notes) {
+  return `${task} ${description} ${dueDate} ${priority} ${notes}`;
+}
+
+//in Project
+function sumProject(projectName, projectDescription, projectDueDate) {
+  return `${projectName}: ${projectDescription} Due:${projectDueDate}`;
+}
+
+//-------------------------------------------------
+//making a project
+function Project(projectName, projectDescription, projectDueDate) {
+  this.projectName = projectName;
+  this.projectDescription = projectDescription;
+  this.projectDueDate = projectDueDate;
+
+  this.info = sumProject(projectName, projectDescription, projectDueDate);
+  //this stands for the object being made, the .names after the this are automatically become keys for the values they store
+  //this.info means return { info: the variable that stands for sumProject(projectName etc, it is shorter yay)}
+}
+
+//---------------------------------------------------
+//eventlistener for projectForm
+//eventlistener for todoForm
+
+//-------------------------------------------
+
+//submit prevent default function
+const projectForm = document.querySelector("#projectForm");
+projectForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  createNewProject();
+  projectForm.style.display = "none";
+  //projectForm.style.display = "block"; to make it re-appear
+});
+
+function createNewProject() {
+  const projectName = projectForm.elements.projectName.value;
+  const projectDescription = projectForm.elements.projectDescription.value;
+  const projectDueDate = projectForm.elements.projectDueDate.value;
+
+  const project = new Project(projectName, projectDescription, projectDueDate);
+  console.log(project.info + ":project as a new object");
+  projectArray.push(project);
+  console.log(
+    projectArray[0].projectName + ":[0].projectName project array after"
+  );
+  console.log(projectArray[0].info + ":[0].info array after");
+}
+
+const todoForm = document.querySelector("#todoForm");
+todoForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  createNewTodo();
+  this.style["display"] = "none";
+});
+
+function createNewTodo() {
+  const task = todoForm.elements.task.value;
+  const description = todoForm.elements.description.value;
+  const dueDate = todoForm.elements.dueDate.value;
+  const priority = projectForm.elements.priority.value;
+  const notes = projectForm.elements.notes.value;
+
+  const todo = new Todo(task, description, dueDate, priority, notes);
+  todoTotalArray.push(todo.todoInfo);
 }
