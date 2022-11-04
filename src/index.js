429980e58ac363896c69df24cf3c4830a9edbabd
Jonathan Shovak <shovakitup@gmail.com>
2022-11-04 14:45:36 -0500

    organized index.js, submitting a project form and todo form will now push an object to the targeted array and hide the form used

--- src/index.js
+++ src/index.js
@@ -1,88 +1,103 @@
-const projectArray = [];
 const todoTotalArray = [];
+const projectArray = [];
+
 //-------------------------------------
-//making a todo
-function Todo(task, description, dueDate, priority, notes) {
+//making a todo and adding it to the todoTotalArray
+const todoForm = document.querySelector("#todoForm");
+
+todoForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const todo = createNewTodo();
+  todoTotalArray.push(todo);
+  todoForm.style.display = "none";
+  //projectForm.style.display = "block";
+});
+
+//--------------------------------------
+//todo functions
+
+function createNewTodo() {
+  const task = todoForm.elements.task.value;
+  const description = todoForm.elements.description.value;
+  const dueDate = todoForm.elements.dueDate.value;
+  let priority = todoForm.elements.priority["value"];
+  const notes = todoForm.elements.notes.value;
+  let todoComplete = todoForm.querySelector("#todoComplete").checked;
+
+  const todo = new Todo(
+    task,
+    description,
+    dueDate,
+    priority,
+    notes,
+    todoComplete
+  );
+
+  return todo;
+}
+
+function Todo(task, description, dueDate, priority, notes, todoComplete) {
   this.task = task;
   this.description = description;
   this.dueDate = dueDate;
   this.priority = priority;
   this.notes = notes;
+  this.todoComplete = todoComplete;
 
-  const sumOfTodo = sumTodo(task, description, dueDate, priority, notes);
-
-  return {
-    todoInfo: sumOfTodo,
-  };
+  this.info = sumTodo(
+    task,
+    description,
+    dueDate,
+    priority,
+    notes,
+    todoComplete
+  );
 }
 
-//--------------------------------------------------
-//functions that will be used many times, in objects made often
-//in Todo
-function sumTodo(task, description, dueDate, priority, notes) {
-  return `${task} ${description} ${dueDate} ${priority} ${notes}`;
-}
-
-//in Project
-function sumProject(projectName, projectDescription, projectDueDate) {
-  return `${projectName}: ${projectDescription} Due:${projectDueDate}`;
-}
-
-//-------------------------------------------------
-//making a project
-function Project(projectName, projectDescription, projectDueDate) {
-  this.projectName = projectName;
-  this.projectDescription = projectDescription;
-  this.projectDueDate = projectDueDate;
-
-  this.info = sumProject(projectName, projectDescription, projectDueDate);
-  //this stands for the object being made, the .names after the this are automatically become keys for the values they store
-  //this.info means return { info: the variable that stands for sumProject(projectName etc, it is shorter yay)}
+function sumTodo(task, description, dueDate, priority, notes, todoComplete) {
+  return `${task} ${description} ${dueDate} ${priority} ${notes} Completed: ${todoComplete}`;
 }
 
 //---------------------------------------------------
-//eventlistener for projectForm
-//eventlistener for todoForm
-
-//-------------------------------------------
-
-//submit prevent default function
+//making a project and adding it to the projectArray
 const projectForm = document.querySelector("#projectForm");
+
 projectForm.addEventListener("submit", (event) => {
   event.preventDefault();
-  createNewProject();
+  const project = createNewProject();
+  projectArray.push(project);
+  console.log(projectArray);
   projectForm.style.display = "none";
   //projectForm.style.display = "block"; to make it re-appear
 });
 
+//------------------------------------------------
+//project functions
 function createNewProject() {
   const projectName = projectForm.elements.projectName.value;
   const projectDescription = projectForm.elements.projectDescription.value;
   const projectDueDate = projectForm.elements.projectDueDate.value;
 
   const project = new Project(projectName, projectDescription, projectDueDate);
-  console.log(project.info + ":project as a new object");
-  projectArray.push(project);
-  console.log(
-    projectArray[0].projectName + ":[0].projectName project array after"
-  );
-  console.log(projectArray[0].info + ":[0].info array after");
+  return project;
 }
 
-const todoForm = document.querySelector("#todoForm");
-todoForm.addEventListener("submit", (event) => {
-  event.preventDefault();
-  createNewTodo();
-  this.style["display"] = "none";
+function Project(projectName, projectDescription, projectDueDate) {
+  this.projectName = projectName;
+  this.projectDescription = projectDescription;
+  this.projectDueDate = projectDueDate;
+
+  this.info = sumProject(projectName, projectDescription, projectDueDate);
+}
+
+function sumProject(projectName, projectDescription, projectDueDate) {
+  return `${projectName}: ${projectDescription} Due:${projectDueDate}`;
+}
+
+//-------------------------------------
+//button toggles
+
+projectButton = document.querySelector("#newProject");
+projectButton.addEventListener("click", (e) => {
+  console.log(e);
 });
-
-function createNewTodo() {
-  const task = todoForm.elements.task.value;
-  const description = todoForm.elements.description.value;
-  const dueDate = todoForm.elements.dueDate.value;
-  const priority = projectForm.elements.priority.value;
-  const notes = projectForm.elements.notes.value;
-
-  const todo = new Todo(task, description, dueDate, priority, notes);
-  todoTotalArray.push(todo.todoInfo);
-}
